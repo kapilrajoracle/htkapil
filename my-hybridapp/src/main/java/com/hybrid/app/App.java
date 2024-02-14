51982c144d0b77994d02c229263270702fa41f42
kapilrajoracle <39433189+kapilrajoracle@users.noreply.github.com>
2024-02-14 09:53:40 +0530

    Add files via upload

---
+++ my-hybridapp/src/main/java/com/hybrid/app/App.java
@@ -0,0 +1,13 @@
+package com.hybrid.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
